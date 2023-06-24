d4560879dc89cca056cb6c092876b3f454d83b4f
Leonardo Costa <112534103+Justice3001@users.noreply.github.com>
2023-06-24 17:08:48 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var timerInterval;
+var generateButton = document.getElementById("generateButton");
+var intervalDropdownItems = document.querySelectorAll(".dropdown-item");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+intervalDropdownItems.forEach(function(item) {
+  item.addEventListener("click", function() {
+    var interval = parseInt(this.getAttribute("data-interval"));
+    startTimer(interval);
+  });
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+generateButton.addEventListener("click", function() {
+  clearInterval(timerInterval);
+  generateCard();
+});
+
+function startTimer(interval) {
+  clearInterval(timerInterval);
+  if (interval !== 0) {
+    timerInterval = setInterval(generateCard, interval);
+  }
+}
+function generateCard() {
+  var cards = [
+    "src/2_of_clubs.png",
+    "src/3_of_diamonds.png",
+    "src/ace_of_spades.png",
+    "src/black_joker.png",
+    "src/jack_of_clubs.png",
+    "src/king_of_diamonds.png",
+    "src/queen_of_hearts.png",
+    "src/red_joker.png",
+    "src/9_of_hearts.png",
+    "src/10_of_spades.png",
+    "src/7_of_clubs.png"
+  ];
+
+  var randomIndex = Math.floor(Math.random() * cards.length);
+  var randomCard = cards[randomIndex];
+
+  var cardContainer = document.getElementById("cardContainer");
+
+  // Remove the existing card if present
+  if (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+
+  var cardImage = document.createElement("img");
+  cardImage.src = randomCard;
+  cardContainer.appendChild(cardImage);
+}
